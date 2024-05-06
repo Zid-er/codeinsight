3cbb95d7319f134533f71a282ad1a2ba24e98c96
Xzide <83851672+Zid-er@users.noreply.github.com>
2024-05-06 03:14:34 -0400

    fix create

---
+++ src/app/create/page.tsx
@@ -0,0 +1,32 @@
+
+
+
+
+const Create = () => {
+    return (
+        <form className="flex flex-col justify-center items-center translate-y-[50%] dark:text-[#ffffff]">
+            <div className="dark:bg-primary bg-slate-50 p-12 rounded w-[50%]">
+                <p className="text-xl font-medium pb-4">Create Insight</p>
+                <div className="flex flex-col">
+                    <label>Title</label>
+                    <input  className="border border-primary outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
+                </div>
+                <div className="flex flex-col">
+                    <label>Description</label>
+                    <textarea className="border border-primary outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" ></textarea>
+                </div>
+                <div className="flex flex-col">
+                    <label>Tag</label>
+                    <input  className="border border-primary outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
+                </div>
+                <div className="flex flex-col">
+                    <label>Code</label>
+                    <input  className="border border-primary outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
+                </div>
+
+            </div>
+        </form>
+    )
+}
+
+export default Create
