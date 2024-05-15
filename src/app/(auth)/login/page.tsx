7518e59f50a50c1a986f0a1b6321137117af957d
cerealexperiments <prettysunshine468@gmail.com>
2024-05-15 08:34:39 +0600

    add react-hook-form and refactor forms with it

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,54 +1,60 @@
 "use client"
-
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { userStore } from "~/stores/general";
 import { Button } from "~/ui/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+}
+
+const loginUser = async (data: Inputs) => {
+  try {
+    const response = await axios.post("/api/login", {
+      ...data
+    }, { withCredentials: true })
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
 
 const Login = () => {
-    const updateUser = userStore((state) => state.updateUser)
-    const router = useRouter()
-
-    const [password, setPassword] = useState<string>("")
-    const [email, setEmail] = useState<string>("")
-    const handleSubmit = async  (e: React.SyntheticEvent) => {
-        e.preventDefault()
-        if (!password || !email) throw Error("Empty fields!")
-        try {
-            const res = await axios(`/api/login`, {
-                method: "POST",
-                withCredentials: true,
-                data: { email: email, password: password},
-            });
-            console.log(res)
-            if (res) {
-                updateUser(res.data.user.payload)
-            }
-            router.replace("/")
-        } catch(err) {
-            console.log("ERR IN LOGIN: ", err)
-            router.replace("/signup")
-        }
+  const { register, handleSubmit } = useForm<Inputs>();
+  const updateUser = userStore((state) => state.updateUser)
+  const router = useRouter()
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const response = await loginUser(data)
+      if (response) {
+        updateUser(response.user.payload)
+      }
+      router.replace("/")
+    } catch (error: any) {
+      console.log('error', error.response)
     }
-    return (
-        <form className="flex flex-col justify-center items-center translate-y-[50%] dark:text-[#ffffff]" onSubmit={handleSubmit}>
-            <div className="dark:bg-primary bg-slate-50 p-12 rounded min-w-[30%] gap-4 flex flex-col">
-                <p className="text-xl font-medium pb-4">Login</p>
-                <div className="flex flex-col">
-                    <label>Email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)}  className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
-                </div>
-                <div className="flex flex-col">
-                    <label>Password</label>
-                    <input value={password} onChange={(e) => setPassword(e.target.value)}  className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" type="password" />
-                </div>
-                <Button>Save</Button>
-                <Link href="/signup" className="text-xs text-blue-500">Signup</Link>
-            </div>
-        </form>
-    );
+  }
+
+  return (
+    <form className="flex flex-col justify-center items-center translate-y-[50%] dark:text-[#ffffff]" onSubmit={handleSubmit(onSubmit)}>
+      <div className="dark:bg-primary bg-slate-50 p-12 rounded min-w-[30%] gap-4 flex flex-col">
+        <p className="text-xl font-medium pb-4">Login</p>
+        <div className="flex flex-col">
+          <label>Email</label>
+          <input {...register("email", { required: true })} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
+        </div>
+        <div className="flex flex-col">
+          <label>Password</label>
+          <input {...register("password", { required: true })} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" type="password" />
+        </div>
+        <Button>Save</Button>
+        <Link href="/signup" className="text-xs text-blue-500">Signup</Link>
+      </div>
+    </form>
+  );
 }
- 
+
 export default Login;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,50 +1,57 @@
 "use client"
-
 import Link from "next/link";
-import { useState } from "react";
 import { Button } from "~/ui/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+
+type Inputs = {
+  email: string;
+  password: string;
+  username: string;
+}
+
+const registerUser = async (data: Inputs) => {
+  try {
+    const response = await axios.post("/api/signup", {
+      ...data
+    }, { withCredentials: true })
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
 
 const Signup = () => {
-    const [password, setPassword] = useState<string>("")
-    const [email, setEmail] = useState<string>("")
-    const [username, setUsername] = useState<string>("")
-    const handleSubmit = async (e: React.SyntheticEvent) => {
-        e.preventDefault()
-        if (!password || !email || !username) throw Error("Empty fields!")
-        await fetch(`/api/signup`, {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password,
-                username: username,
-            }),
-        });
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const response = await registerUser(data)
+      console.log(response)
+    } catch (error: any) {
+      console.log('error', error.response)
     }
-    return (
-        <form className="flex flex-col justify-center items-center translate-y-[30%] dark:text-[#ffffff]" onSubmit={handleSubmit}>
-            <div className="dark:bg-primary bg-slate-50 p-12 rounded min-w-[30%] gap-4 flex flex-col">
-                <p className="text-xl font-medium pb-4">Signup</p>
-                <div className="flex flex-col">
-                    <label>Username</label>
-                    <input value={username} onChange={(e) => setUsername(e.target.value)} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
-                </div>
-                <div className="flex flex-col">
-                    <label>Email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
-                </div>
-                <div className="flex flex-col">
-                    <label>Password</label>
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" type="password" />
-                </div>
-                <Button>Save</Button>
-                <Link href="/login" className="text-xs text-blue-500">Login</Link>
-            </div>
-        </form>
-    );
+  }
+  return (
+    <form className="flex flex-col justify-center items-center translate-y-[30%] dark:text-[#ffffff]" onSubmit={handleSubmit(onSubmit)}>
+      <div className="dark:bg-primary bg-slate-50 p-12 rounded min-w-[30%] gap-4 flex flex-col">
+        <p className="text-xl font-medium pb-4">Signup</p>
+        <div className="flex flex-col">
+          <label>Username</label>
+          <input {...register("username", { required: true })} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
+        </div>
+        <div className="flex flex-col">
+          <label>Email</label>
+          <input {...register("email", { required: true })} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" />
+        </div>
+        <div className="flex flex-col">
+          <label>Password</label>
+          <input {...register("password", { required: true })} className="border border-[#858585] outline-none px-2 py-2 rounded dark:border-[#2d2d2d] bg-transparent" type="password" />
+        </div>
+        <Button>Save</Button>
+        <Link href="/login" className="text-xs text-blue-500">Login</Link>
+      </div>
+    </form>
+  );
 }
 
 export default Signup;
